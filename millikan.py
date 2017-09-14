f2c8c2e559a9cdb7eca5c7196446a99284373439
kstoreyf <kstoreyfisher@gmail.com>
2017-09-13 22:56:15 -0400

    calculated h, made plot nicer

--- millikan.py
+++ millikan.py
@@ -18,17 +18,27 @@ Exy = 1/N * np.sum(freqs*volts)
 
 m = (Exy - Ex*Ey)/(Exx - Ex**2)
 c = (Exx*Ey - Ex*Exy)/(Exx - Ex**2)
-print 'Slope: ', m
-print 'Intercept: ', c
+print 'Slope:', m
+print 'Intercept:', c
 
 ys_lstsqs = []
 for f in freqs:
     y = m*f + c
     ys_lstsqs.append(y)
 
-plt.plot(freqs, volts, marker='o', linestyle='', color='cyan', markeredgewidth=0)
-plt.plot(freqs, ys_lstsqs, marker='', linestyle='-', color='purple')
+e = 1.602e-19  # Charge of electron in Coulombs
+h = m*e
+print r'h =', h
+realh = 6.626e-34
+print '% from accepted h:',abs(realh-h)/realh
+
+plt.plot(freqs, volts, marker='o', linestyle='', color='cyan', markeredgewidth=0,
+         label='Millikan experimental data')
+plt.plot(freqs, ys_lstsqs, marker='', linestyle='-', color='purple',
+         label='Least squares fit')
 plt.xlabel('Frequency (Hz)')
 plt.ylabel('Voltage (V)')
+plt.legend(loc='upper left', frameon=False, prop={'size':12})
+plt.ylim(0, 3.5)
 plt.show()
 
