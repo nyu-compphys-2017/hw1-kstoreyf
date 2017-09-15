564199607bb2218c0d0bc6684909c6846645e62c
kstoreyf <kstoreyfisher@gmail.com>
2017-09-14 22:09:12 -0400

    added colorbar to plots

--- mandelbrot.py
+++ mandelbrot.py
@@ -22,7 +22,7 @@ def iterate(c, xmax):
     return False, counter
 
 # Create image using the Mandelbrot fractal using an NxN grid
-def image(N=1000):
+def image(N=100):
     xmax = 2
     xmin = -xmax
     ns = np.linspace(xmin, xmax, N)
@@ -42,10 +42,12 @@ def image(N=1000):
             else:
                 outsetx.append(x)
                 outsety.append(y)
-                outsetcols.append(num)
+                outsetcols.append(np.log10(num+1))
 
     plt.scatter(insetx, insety, marker='.', edgecolor='', c='black')
     plt.scatter(outsetx, outsety, marker='.', edgecolor='', c=outsetcols, cmap=cm.plasma)
+    cbar = plt.colorbar()
+    cbar.set_label('log(# of iterations)')
 
     plt.xlim(-2, 2)
     plt.ylim(-2, 2)
